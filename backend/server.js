84ae0cc22d4d5219ab1c0f689f4dcded1a00965c
Maissa-benyaala <maissa.benyaala@gmail.com>
2023-11-16 22:59:17 +0100

    stripe integration

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,34 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+const stripeSecretKey =
+  process.env.STRIPE_SECRET_KEY ||
+  "sk_test_51OD8M6K1RjpevDg2WdFS71a2pFvY1xBStagqxtxGxO6C5DqzZrxb39gYSJoMuUZS1JDGJVzeuDhrprcKpPwzvFZU00aKHC34Ms";
+const stripe = require("stripe")(stripeSecretKey);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use(cors());
+
+app.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price: "price_1ODCg8K1RjpevDg2Aieut0qQ",
+        quantity: 1,
+      },
+    ],
+    mode: "subscription",
+    success_url: "http://localhost:3000/success",
+    cancel_url: "http://localhost:3000/cancel",
+  });
+
+  res.json({ id: session.id });
 });
 
 app.listen(PORT, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { loadStripe } from "@stripe/stripe-js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const stripePromise = loadStripe(
+  "pk_test_51OD8M6K1RjpevDg2q6WULErzcied6ONalFrSkMc1xlaLe7akbHZ9lz7K8VV5DQbG1rkQjJiZYjwHIrsrvFWn5SFZ00VzJXIyc1"
+);
+
+class App extends Component {
+  handleSubscription = async () => {
+    const stripe = await stripePromise;
+
+    const response = await fetch(
+      "http://localhost:5000/create-checkout-session",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const session = await response.json();
+
+    const result = await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+
+    if (result.error) {
+      console.error(result.error.message);
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Subscription Page</h2>
+        <button onClick={this.handleSubscription}>Subscribe</button>
+      </div>
+    );
+  }
 }
 
 export default App;
